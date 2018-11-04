6bd2a628d5488fce87500628c22e84f3fbf947a1
Yury V. Zaytsev <yury.zaytsev@moneymeets.com>
2018-11-04 20:00:00 +0100

    Implement clone entry function to ease recording traffic patterns

--- logbook/admin.py
+++ logbook/admin.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 
 from .models import Aerodrome, Aircraft, Certificate, LogEntry, Pilot
 
+
+class LogEntryAdmin(admin.ModelAdmin):
+    save_as = True
+
+
 admin.site.register(Aerodrome)
 admin.site.register(Aircraft)
 admin.site.register(Certificate)
-admin.site.register(LogEntry)
+admin.site.register(LogEntry, LogEntryAdmin)
 admin.site.register(Pilot)
