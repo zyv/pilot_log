40b5c33799daebce3eb55d0ba8c2b886514eaf8a
Yury V. Zaytsev <yury.zaytsev@moneymeets.com>
2018-11-04 20:10:24 +0100

    Turn aerodrome entry fields into cool autocompleted select2 boxes

--- logbook/admin.py
+++ logbook/admin.py
@@ -3,11 +3,16 @@ from django.contrib import admin
 from .models import Aerodrome, Aircraft, Certificate, LogEntry, Pilot
 
 
+class AerodromeAdmin(admin.ModelAdmin):
+    search_fields = ("name", "icao_code")
+
+
 class LogEntryAdmin(admin.ModelAdmin):
+    autocomplete_fields = ("from_aerodrome", "to_aerodrome")
     save_as = True
 
 
-admin.site.register(Aerodrome)
+admin.site.register(Aerodrome, AerodromeAdmin)
 admin.site.register(Aircraft)
 admin.site.register(Certificate)
 admin.site.register(LogEntry, LogEntryAdmin)
