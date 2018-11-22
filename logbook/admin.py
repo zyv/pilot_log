d396f7eea24e23e8c31b8f28b23d68dbea1feeb7
Yury V. Zaytsev <yury.zaytsev@moneymeets.com>
2018-11-22 20:00:21 +0100

    Add log entries filtering by pilot

--- logbook/admin.py
+++ logbook/admin.py
@@ -22,7 +22,7 @@ class LogEntryAdmin(admin.ModelAdmin):
         "launch_type": admin.VERTICAL,
     }
     list_display = ("get_time", "get_registration", "get_from", "get_to", "pilot", "copilot")
-    list_filter = ("time_function",)
+    list_filter = ("time_function", "pilot")
     save_as = True
 
     def get_time(self, obj):
