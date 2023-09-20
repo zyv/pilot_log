90f15017af3d6e881705ad4ef3f33be07eb0230a
Yury V. Zaytsev <yury.zaytsev@moneymeets.com>
2023-09-20 21:01:39 +0200

    feat(models): add self flag on the pilot model

--- logbook/models.py
+++ logbook/models.py
@@ -65,6 +65,8 @@ class Pilot(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=150)
 
+    self = models.BooleanField(default=False)
+
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
