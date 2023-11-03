434bf9089127ad2632c6d4512e3f7956594bd95a
Yury V. Zaytsev <yury.zaytsev@moneymeets.com>
2023-11-03 12:19:37 +0100

    fix(dashboard): escape certificate names to prevent HTML injection

--- logbook/views/utils.py
+++ logbook/views/utils.py
@@ -4,6 +4,7 @@ from dateutil.relativedelta import relativedelta
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
+from django.utils.html import escape
 from django.utils.safestring import mark_safe
 from django.views.generic import ListView, TemplateView
 
@@ -14,27 +15,21 @@ EXPIRY_WARNING_THRESHOLD = relativedelta(months=3)
 
 def check_certificates_expiry(request):
     today = datetime.now(tz=UTC).date()
-    certificates = Certificate.objects.filter(
+    for certificate in Certificate.objects.filter(
         valid_until__lte=today + EXPIRY_WARNING_THRESHOLD,
         supersedes_set=None,
-    )
-    if certificates.exists():
-        for certificate in certificates:
-            if not certificate.valid:
-                messages.error(
-                    request,
-                    mark_safe(
-                        f'Certificate <strong>"{certificate}"</strong> has expired on {certificate.valid_until}!',
-                    ),
-                )
-            else:
-                messages.warning(
-                    request,
-                    mark_safe(
-                        f'Certificate <strong>"{certificate}"</strong> expires in '
-                        f"<strong>{(certificate.valid_until - today).days}</strong> days!",
-                    ),
-                )
+    ):
+        designation = f'Certificate <strong>"{escape(certificate.name)}"</strong> issued on {certificate.issue_date}'
+        if not certificate.valid:
+            messages.error(
+                request,
+                mark_safe(f"{designation} has expired on {certificate.valid_until}!"),
+            )
+        else:
+            messages.warning(
+                request,
+                mark_safe(f"{designation} expires in <strong>{(certificate.valid_until - today).days}</strong> days!"),
+            )
 
 
 class AuthenticatedView(UserPassesTestMixin, LoginRequiredMixin):
