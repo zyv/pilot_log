2029f21f450a0f32df658b9f5bfffe1cb4e5e660
Yury V. Zaytsev <yury@shurup.com>
2025-03-04 13:08:03 +0100

    fix(experience): fix type annotation in CPL requirements

--- logbook/statistics/experience.py
+++ logbook/statistics/experience.py
@@ -3,6 +3,8 @@ from collections.abc import Iterable
 from dataclasses import dataclass
 from datetime import timedelta
 
+from django.db.models import QuerySet
+
 from ..models.aircraft import AircraftType
 from ..models.log_entry import FunctionType, LogEntry
 
@@ -55,7 +57,7 @@ def compute_totals(entries: Iterable["LogEntry"], full_stop=False) -> TotalsReco
     )
 
 
-def cpl_entry_requirements(entries: Iterable["LogEntry"]):
+def cpl_entry_requirements(entries: QuerySet["LogEntry"]):
     max_glider_credit = timedelta(hours=10)
     glider_time = flight_time(
         entries.filter(time_function=FunctionType.PIC, aircraft__type__in=(AircraftType.GLD, AircraftType.TMG)),
