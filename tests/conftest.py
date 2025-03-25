501d368a0fe20157e0b0473a85fdc5e6f8b0ef8d
Yury V. Zaytsev <yury@shurup.com>
2025-03-25 12:23:39 +0100

    test(statistics): add test for `compute_totals`

---
+++ tests/conftest.py
@@ -0,0 +1,49 @@
+from datetime import UTC, datetime, timedelta
+
+import pytest
+
+from logbook.models.aerodrome import Aerodrome
+from logbook.models.aircraft import Aircraft, AircraftType
+from logbook.models.log_entry import LogEntry, LogEntryQuerySet
+from logbook.models.pilot import Pilot
+
+NUMBER_OF_LOG_ENTRIES = 30
+
+
+@pytest.fixture
+def log_entries() -> LogEntryQuerySet[LogEntry]:
+    aircraft = Aircraft.objects.create(
+        type=AircraftType.SEP,
+        maker="Test",
+        model="Test",
+        icao_designator="TEST",
+        registration="TEST",
+    )
+    aerodrome = Aerodrome.objects.create(
+        name="Test",
+        city="Test",
+        country="DE",
+        icao_code="TEST",
+        latitude=0,
+        longitude=0,
+        elevation=0,
+        priority=0,
+    )
+    pilot = Pilot.objects.create(first_name="Test", last_name="Test")
+
+    for i in range(NUMBER_OF_LOG_ENTRIES):
+        LogEntry.objects.create(
+            aircraft=aircraft,
+            from_aerodrome=aerodrome,
+            to_aerodrome=aerodrome,
+            departure_time=datetime.now(tz=UTC) - timedelta(days=110 - i),
+            arrival_time=datetime.now(tz=UTC) - timedelta(days=110 - i) + timedelta(minutes=1),
+            landings=1,
+            pilot=pilot,
+        )
+
+    first_entry = LogEntry.objects.get(pk=1)
+    first_entry.landings = 3
+    first_entry.save()
+
+    return LogEntry.objects.all()
--- tests/test_statistics.py
+++ tests/test_statistics.py
@@ -2,50 +2,35 @@ from datetime import UTC, datetime, timedelta
 
 import pytest
 
-from logbook.models.aerodrome import Aerodrome
-from logbook.models.aircraft import Aircraft, AircraftType
 from logbook.models.log_entry import LogEntry
-from logbook.models.pilot import Pilot
 from logbook.statistics.currency import CurrencyStatus, get_rolling_currency
+from logbook.statistics.experience import compute_totals
 
-NUMBER_OF_LOG_ENTRIES = 30
-
-
-@pytest.fixture
-def log_entries():
-    aircraft = Aircraft.objects.create(
-        type=AircraftType.SEP,
-        maker="Test",
-        model="Test",
-        icao_designator="TEST",
-        registration="TEST",
-    )
-    aerodrome = Aerodrome.objects.create(
-        name="Test",
-        city="Test",
-        country="DE",
-        icao_code="TEST",
-        latitude=0,
-        longitude=0,
-        elevation=0,
-        priority=0,
-    )
-    pilot = Pilot.objects.create(first_name="Test", last_name="Test")
-
-    for i in range(NUMBER_OF_LOG_ENTRIES):
-        LogEntry.objects.create(
-            aircraft=aircraft,
-            from_aerodrome=aerodrome,
-            to_aerodrome=aerodrome,
-            departure_time=datetime.now(tz=UTC) - timedelta(days=110 - i),
-            arrival_time=datetime.now(tz=UTC) - timedelta(days=110 - i) + timedelta(seconds=1),
-            landings=1,
-            pilot=pilot,
-        )
+from .conftest import NUMBER_OF_LOG_ENTRIES
 
 
 @pytest.mark.django_db
-def test_not_current():
+def test_compute_totals_total_landings(log_entries):
+    totals = compute_totals(log_entries)
+    assert totals.landings == NUMBER_OF_LOG_ENTRIES + 2
+
+
+@pytest.mark.django_db
+def test_compute_totals_full_stop_landings(log_entries):
+    totals = compute_totals(log_entries, full_stop=True)
+    assert totals.landings == NUMBER_OF_LOG_ENTRIES
+
+
+@pytest.mark.django_db
+def test_compute_totals_time(log_entries):
+    totals = compute_totals(log_entries)
+    assert totals.time.total_seconds() == pytest.approx(
+        (timedelta(minutes=1) * NUMBER_OF_LOG_ENTRIES).total_seconds(), abs=0.1
+    )
+
+
+@pytest.mark.django_db
+def test_not_current():  # no fixture!
     currency = get_rolling_currency(LogEntry.objects.all(), 5)
 
     assert currency.expires_in == timedelta(days=0)
@@ -56,10 +41,9 @@ def test_not_current():
 
 @pytest.mark.django_db
 def test_current_linear(log_entries):
-    currency = get_rolling_currency(LogEntry.objects.all(), 5)
+    currency = get_rolling_currency(log_entries, 5)
 
     assert currency.expires_in.total_seconds() == pytest.approx(timedelta(days=5).total_seconds(), abs=0.1)
-
     assert currency.expires_on == (datetime.now(tz=UTC) + timedelta(days=5 - 1)).date()
     assert currency.status == CurrencyStatus.EXPIRING
     assert currency.landings_to_renew == 1
@@ -74,10 +58,9 @@ def test_current_first_current_and_expired_on_same_day(log_entries):
     first_current_entry.arrival_time -= timedelta(hours=1)
     first_current_entry.save()
 
-    currency = get_rolling_currency(LogEntry.objects.all(), 5)
+    currency = get_rolling_currency(log_entries, 5)
 
     assert currency.expires_in.total_seconds() == pytest.approx(timedelta(days=8, hours=23).total_seconds(), abs=0.1)
-
     assert currency.expires_on == (datetime.now(tz=UTC) + timedelta(days=8)).date()
     assert currency.status == CurrencyStatus.EXPIRING
     assert currency.landings_to_renew == 5
